b84c229d68c0705b82cb8a4d0dd02e7b885062bc
sona91shoney <108096038+sona91shoney@users.noreply.github.com>
2022-07-10 19:17:53 -0400

    Initial commit

---
+++ src/DragonCave.java
@@ -0,0 +1,38 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class DragonCave {
+    public static void main(String[] args) {
+        System.out.println("You are in a land full of dragons. In front of you, \n" +
+                "you see two caves....\n" +
+                "In one cave," + "the dragon is friendly \n " +
+                "and will share his treasure with you." + "The other dragon \n" +
+                "is greedy and hungry and will eat you on sight.");
+        System.out.println("Which cave will you go into? (1 or 2)");
+        System.out.println("--------------------------------------");
+        Scanner xx = new Scanner(System.in);
+        // Sample content here committed via git
+        try {
+            int option = xx.nextInt();
+
+
+            if (option == 1) { // If user selects cave 1
+                System.out.println("You approach the cave...\n" +
+                        "It is dark and spooky...\n" +
+                        "A large dragon jumps out in front of you! He opens his jaws and ...\n" +
+                        "Gobbles you down in one bite!");
+            } else if (option == 2) { // If user selects cave 2
+                System.out.println("Friendly Dragon and would like to share his treasure with you!");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Number format exception " + e.getMessage());
+        } catch (InputMismatchException e) {
+            System.out.println("Input missmatch exception " + e.getMessage());
+        }
+    }
+}
+
+
+
+
+
